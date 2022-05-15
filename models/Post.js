85ef35a12afd0e60d92aee1764beb6338867af49
Weston Peters <westonrpr@yahoo.com>
2022-05-15 18:02:00 -0500

    add new field for 'private' posts

--- models/Post.js
+++ models/Post.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const { requiredStr } = require('./fieldTypes');
+const { requiredStr, requiredBool } = require('./fieldTypes');
 const PostSchema = new Schema(
   {
     author: requiredStr,
     title: requiredStr,
     body: requiredStr,
     category: requiredStr,
+    private: requiredBool,
     keywords: [{ type: String }],
   },
   { timestamps: true }
