2360c41be71af1fa2bfb7db2ccb3de4cd812f593
Weston Peters <westonrpr@yahoo.com>
2022-05-15 19:20:14 -0500

    add routes to create, update, read, delete

--- routes/post.js
+++ routes/post.js
@@ -1,24 +1,32 @@
 const express = require('express');
-const res = require('express/lib/response');
+const mongoose = require('mongoose');
 const verifyUser = require('../middleware/auth');
 const router = express.Router();
 const User = require('../models/User');
-const { server_error } = require('../util/responseTypes');
+const {
+  server_error,
+  resource_created,
+  resource_deleted,
+  resource_updated,
+} = require('../util/responseTypes');
+
 // ROUTE POST api/post/create
 // DESC create a new blog post
 // ACCESS private
 router.post('/create', verifyUser, async (req, res) => {
-  const { username, title, body, category, keywords } = req.body;
+  const { author, title, body, category, private, keywords } = req.body;
+  console.log(req.user.id);
   try {
     const newPost = {
       author,
       title,
       body,
       category,
+      private,
       keywords,
     };
-    const user = await User.findOneAndUpdate(
-      { username: username },
+    const user = await User.findByIdAndUpdate(
+      req.user.id,
       {
         $push: { posts: newPost },
       },
@@ -32,58 +40,68 @@ router.post('/create', verifyUser, async (req, res) => {
     console.log(err);
   }
 });
-// ROUTE PUT api/post/edit/
+
+// ROUTE PUT api/post/edit
 // DESC edit a blog post
 // ACCESS private
 router.put('/edit', verifyUser, async (req, res) => {
-  const { username, title, body, category, keywords, postId } = req.body;
+  const { author, title, body, category, private, keywords } = req.body;
+  const postId = mongoose.Types.ObjectId(req.body.postId);
   const updatedPost = {
-    author: username,
+    author,
     title,
     body,
     category,
+    private,
     keywords,
   };
   try {
     const user = await User.findOneAndUpdate(
-      { username: username, 'posts._id': postId },
+      { _id: req.user.id, 'posts._id': postId },
       {
-        $set: { 'posts.$': updatedPost },
+        $set: {
+          'posts.$.author': author,
+          'posts.$.title': title,
+          'posts.$.body': body,
+          'posts.$.category': category,
+          'posts.$.private': private,
+          'posts.$.keywords': keywords,
+        },
       },
-      { returnOriginal: false }
+      { returnOriginal: false, setDefaultsOnInsert: false }
     );
-    // we'll figure out what to send here
-    res.status.json({ whatSend: "don't know" });
+    res.status(200).json(resource_updated);
   } catch (err) {
     res.status(500).json(server_error);
     console.log(err);
   }
 });
-// ROUTE GET api/post/all/:username
+
+// ROUTE GET api/post/delete
 // DESC get all posts
 // ACCESS private
-router.get('/all/:username', verifyUser, async (req, res) => {
-  const { username } = req.params;
+router.get('/all', verifyUser, async (req, res) => {
+  const userId = req.user.id;
   try {
-    const user = await User.findOne({ username: username });
+    const user = await User.findById(userId);
     res.status(200).json(user.posts);
   } catch (err) {
     res.status(500).json(server_error);
     console.log(err);
   }
 });
-// ROUTE DELETE api/post/delete/:username/:id
+
+// ROUTE DELETE api/post/delete/:id
 // DESC delete a single blog post
 // ACCESS private
-router.delete('delete/:username/:id', verifyUser, (req, res) => {
-  const { username, id } = req.params;
-
+router.delete('/delete/:id', verifyUser, async (req, res) => {
+  const postId = req.params.id;
+  console.log(postId);
   try {
-    const user = User.findOneAndUpdate(
-      { username: username },
-      // id might need to be converted into an object
-      { $pull: { posts: { _id: id } } },
-      { returnOriginal: true }
+    const user = await User.findOneAndUpdate(
+      { _id: req.user.id },
+      { $pull: { posts: { _id: postId } } },
+      { returnOriginal: false }
     );
     res.status(200).json(user.posts);
   } catch (err) {
