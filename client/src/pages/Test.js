90a9dc8df3abf6918b52d7a62933fdc9c7b6f349
Weston Peters <westonrpr@yahoo.com>
2022-05-17 11:00:32 -0500

    import and visually inspect Create page component

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -1,9 +1,10 @@
 import { Container } from '@chakra-ui/react';
 import Login from '../pages/Login';
+import Create from './Create';
 const Test = () => {
   return (
     <Container maxW="800px" centerContent={true} mt="8em">
-      <Login />
+      <Create />
     </Container>
   );
 };
