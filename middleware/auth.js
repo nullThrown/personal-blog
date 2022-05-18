fb417adfb1bd0d5948a7a75a9a3082d7729b7742
Weston Peters <westonrpr@yahoo.com>
2022-05-18 10:33:16 -0500

    response message change

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken');
-const { unauthenticated } = require('../util/responseTypes');
+const { token_invalid } = require('../util/responseTypes');
 
 const verifyToken = (req, res, next) => {
   const token = req.header('x_auth_token');
 
   if (!token) {
-    return res.status(400).json({ error: unauthenticated });
+    return res.status(401).json(token_invalid);
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWTSECRET);
+    console.log(decoded);
     req.user = decoded.user;
   } catch (err) {
-    return res.status(400).json({ error: unauthenticated });
+    return res.status(401).json(token_invalid);
   }
   next();
 };
