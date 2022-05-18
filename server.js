d30dbc18331b6227da2ee63fbce0838b428d4ce7
Weston Peters <westonrpr@yahoo.com>
2022-05-18 10:34:07 -0500

    fix syntax error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 mongooseConnect();
 
 const corsOptions = {
-  origin: 'http://localHost:3000',
+  origin: 'http://localhost:3000',
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
