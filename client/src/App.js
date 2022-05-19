b8025a5fedbe66b14e7120e66d0c94258ef22d95
Weston Peters <westonrpr@yahoo.com>
2022-05-19 13:58:06 -0500

    add react-query provider & dev tools

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import AppRoutes from './routes/index';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Router>
-        <AppRoutes />
-      </Router>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Router>
+          <AppRoutes />
+        </Router>
+      </ChakraProvider>
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   );
 }
 
