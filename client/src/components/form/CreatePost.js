34c8f9ec60abe8c169ca44c2efca48058d804e2a
Weston Peters <westonrpr@yahoo.com>
2022-05-27 13:52:07 -0500

    add react-query mutation to posts and incorporate success sideeffects

--- client/src/components/form/CreatePost.js
+++ client/src/components/form/CreatePost.js
@@ -7,11 +7,16 @@ import {
   Textarea,
   Button,
   Flex,
+  Box,
+  Text,
+  Center,
+  VStack,
 } from '@chakra-ui/react';
+import ErrorMsg from '../typography/ErrorMsg';
 import { useState } from 'react';
 import { useMutation } from 'react-query';
 import { createPost } from '../../api/posts';
-
+import queryClient from '../../config/queryClient';
 const CreatePost = () => {
   const [post, setPost] = useState({
     author: 'hades',
@@ -24,22 +29,52 @@ const CreatePost = () => {
   const mutatePost = useMutation(post => createPost(post), {
     onSuccess: data => {
       console.log(data);
+      queryClient.invalidateQueries('posts');
     },
-    onError: data => {
-      console.log(data);
+    onError: error => {
+      console.log(error.msg);
     },
   });
 
   const onInputChange = e => {
     setPost({ ...post, [e.target.name]: e.target.value });
   };
-  const handleSubmit = () => {};
+  const handleSubmit = e => {
+    e.preventDefault();
+    mutatePost.mutate(post);
+  };
+  if (mutatePost.isSuccess) {
+    return (
+      <VStack spacing="20px">
+        <Flex
+          w="100%"
+          p="14px 0"
+          bg="green.400"
+          borderRadius="10px"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text color="white" fontSize="xl">
+            Post Created Successfully!
+          </Text>
+        </Flex>
+        <Button
+          type="button"
+          onClick={() => mutatePost.reset()}
+          size="lg"
+          colorScheme="blackAlpha"
+        >
+          Add Another Post
+        </Button>
+      </VStack>
+    );
+  }
   return (
     <FormControl as="form" mt="2em" mb="10em">
       <Heading size="lg" fontWeight="500" textAlign="center">
         Add New Post
       </Heading>
-
+      {mutatePost.isError && <ErrorMsg msg={mutatePost.error.msg} />}
       <FormLabel htmlFor="title">Title</FormLabel>
       <Input
         variant="flushed"
@@ -71,12 +106,11 @@ const CreatePost = () => {
       />
       <Flex justifyContent="center" mt="2em">
         <Button
-          type="button"
+          type="submit"
           size="lg"
           colorScheme="green"
-          onClick={() => {
-            mutatePost.mutate(post);
-          }}
+          onClick={handleSubmit}
+          isLoading={mutatePost.isLoading}
         >
           Submit
         </Button>
