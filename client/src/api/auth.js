05d2d3cf5eff71c9844fcc37c09cb2684daba07e
Weston Peters <westonrpr@yahoo.com>
2022-05-28 17:53:16 -0500

    add two api calls for logout and reauth user
    *logout func might not need an API call. It would only require a removal of the token fron LS. However, if I wanted to invalidate or blacklist that users token (which I should) then it would be necessary. *reauth user is for allowing an already logged in user to authenticate for account sensitive actions (changing settings)

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -9,3 +9,22 @@ export const loginUser = async user => {
     return Promise.reject(new Error(err));
   }
 };
+export const logoutUser = async user => {
+  try {
+    const res = await axios.post('/auth/login', user);
+    return res;
+  } catch (err) {
+    return Promise.reject(new Error(err));
+  }
+};
+// further authorizes actions for a already authed(logged in) user
+// e.g., changing one's password or other sensitive account information
+// this action requires that a user is already authed (client has token)
+export const reAuthUser = async password => {
+  try {
+    const res = await axios.post('/auth/reauth', { password });
+    return res.data;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
