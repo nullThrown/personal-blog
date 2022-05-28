323820071734efa3267dfbfb415c54e09d3f31bb
Weston Peters <westonrpr@yahoo.com>
2022-05-28 17:53:54 -0500

    add account link icon

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,12 +1,17 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Flex, Icon, Link } from '@chakra-ui/react';
+import { BsPersonBoundingBox } from 'react-icons/bs';
+import { Link as RouterLink } from 'react-router-dom';
 import Banner from '../misc/Banner';
 import MainNav from '../nav/MainNav';
 const Header = ({ bannerImg, headingTxt }) => {
   return (
     <Box as="header">
-      <Box m="1em">
+      <Flex alignItems="end" m="0 auto 1em" width="98%">
         <Heading w="100%">{headingTxt}</Heading>
-      </Box>
+        <Link as={RouterLink} to="/account">
+          <Icon as={BsPersonBoundingBox} boxSize="28px" color="blue.800"></Icon>
+        </Link>
+      </Flex>
       <Banner src={bannerImg} />
       <MainNav />
     </Box>
