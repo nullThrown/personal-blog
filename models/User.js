decda092bdb78bba5ed376f5ec44f3ad9a9eb328
Weston Peters <westonrpr@yahoo.com>
2022-05-30 17:09:36 -0500

    import change & add new field -- categories

--- models/User.js
+++ models/User.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const PostSchema = require('./Post').schema;
+const PostSchema = require('./Post');
 const { requiredStr } = require('./fieldTypes');
 const UserSchema = new Schema(
   {
     username: { ...requiredStr, unique: true },
     email: { ...requiredStr, unique: true },
     password: requiredStr,
+    categories: [
+      {
+        type: String,
+      },
+    ],
     posts: [PostSchema],
   },
   { timestamps: true }
