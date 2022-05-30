2d7d7b62d9e268250d5c67158b22821c2f4301b1
Weston Peters <westonrpr@yahoo.com>
2022-05-30 17:08:23 -0500

    add func to account -- edit and layout

--- client/src/components/layout/AccountInfo.js
+++ client/src/components/layout/AccountInfo.js
@@ -3,7 +3,6 @@ import {
   FormLabel,
   FormErrorMessage,
   FormHelperText,
-  input,
   Input,
   VStack,
   Box,
@@ -14,51 +13,81 @@ import {
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import { useMutation } from 'react-query';
-import axios from '../../config/axios';
 import { reAuthUser } from '../../api/auth';
-
+const categories = [
+  {
+    name: 'sex',
+    id: '3432',
+  },
+  {
+    name: 'drugs',
+    id: '3233324',
+  },
+  {
+    name: 'rock & roll',
+    id: '5678',
+  },
+];
 const AccountInfo = () => {
-  const [user, setUser] = useState({
+  const [credentials, setCredentials] = useState({
     username: 'hades',
-    categories: [
-      {
-        name: 'sex',
-        id: '3432',
-      },
-      {
-        name: 'drugs',
-        id: '3233324',
-      },
-      {
-        name: 'rock & roll',
-        id: '5678',
-      },
-    ],
     password: '',
+    confirmPassword: '',
   });
+  const [newCategory, setNewCategory] = useState('');
 
   const [isEditLocked, setIsEditLocked] = useState(true);
   const [passwordUnlock, setPasswordUnlock] = useState('');
+
   const editUnlock = useMutation(passwordUnlock => reAuthUser(passwordUnlock), {
     onSuccess: data => {
       console.log(data);
       setIsEditLocked(false);
     },
     onError: error => {
-      console.log(error);
+      // console.log(error);
+      console.log('the mutation is now in an error state');
+
       if (error.response.data.msg === 'invalid_credentials') {
         console.log('wrong passsword bozo');
       }
-      if (error.responses.data.msg === 'server_error') {
+      if (error.response.data.msg === 'server_error') {
         console.log('server error dude');
       }
-
-      // set error msg component to display error
-      // should only be two types : wrong password || server error
     },
   });
-  const onInputChange = () => {};
+  const onInputChange = e => {
+    switch (e.target.name) {
+      case 'passwordUnlock':
+        setPasswordUnlock(e.target.value);
+        break;
+      case 'username':
+        setCredentials({ ...credentials, username: e.target.value });
+        break;
+      case 'password':
+        setCredentials({ ...credentials, password: e.target.value });
+        break;
+      case 'confirmPassword':
+        setCredentials({ ...credentials, confirmPassword: e.target.value });
+        break;
+      case 'category':
+        setNewCategory(e.target.value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
   const handlePasswordUnlock = e => {
+    e.preventDefault();
+    editUnlock.mutate(passwordUnlock);
+  };
+  const handleCredentials = e => {
+    e.preventDefault();
+    editUnlock.mutate(passwordUnlock);
+  };
+  const handleNewCategory = e => {
     e.preventDefault();
     editUnlock.mutate(passwordUnlock);
   };
@@ -69,12 +98,12 @@ const AccountInfo = () => {
           You will not be able to edit your username, change your password, or
           add new categories until you enter your password below.
         </Text>
-        <FormLabel htmlFor="password-unlock">Password</FormLabel>
+        <FormLabel htmlFor="passwordUnlock">Password</FormLabel>
         <Flex>
           <Input
             type="password"
             name="passwordUnlock"
-            id="password-unlock"
+            id="passwordUnlock"
             placeholder="unlock edit..."
             onChange={e => setPasswordUnlock(e.target.value)}
           />
@@ -83,6 +112,7 @@ const AccountInfo = () => {
             onClick={handlePasswordUnlock}
             colorScheme="blue"
             ml=".5em"
+            isLoading={editUnlock.isLoading}
           >
             Unlock
           </Button>
@@ -92,35 +122,59 @@ const AccountInfo = () => {
         ) : (
           <Text>Edit is free!!</Text>
         )}
+        {editUnlock.isError && (
+          <Text>the mutation is now in an error state</Text>
+        )}
       </Box>
-      <VStack alignItems="left" spacing="32px" maxW="340px">
-        <Box>
+      <Flex alignItems="start" justifyContent="space-evenly" mb="10em">
+        <Box p="2em" boxShadow="5px 5px 15px rgb(240,240,240)">
+          <Text textAlign="center" fontSize="xl" fontWeight="400">
+            Credentials
+          </Text>
           <FormLabel htmlFor="username">Username</FormLabel>
           <Input
             id="username"
             name="username"
-            value={user.username}
+            value={credentials.username}
+            isReadOnly={isEditLocked}
             onChange={onInputChange}
           />
-        </Box>
-        <Box>
           <FormLabel htmlFor="password">Password</FormLabel>
           <Input
             type="password"
             id="password"
             name="password"
-            value={user.password}
+            value={credentials.password}
+            isReadOnly={isEditLocked}
+            onChange={onInputChange}
+          />
+          <FormLabel htmlFor="password">Confirm Password</FormLabel>
+          <Input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={credentials.confirmPassword}
+            isReadOnly={isEditLocked}
             onChange={onInputChange}
           />
         </Box>
-        <Box mt="10em">
-          <FormLabel htmlFor="category"></FormLabel>
-          <Input type="" />
-          {user.categories.map(cat => {
+        <Box p="2em" boxShadow="5px 5px 15px rgb(240,240,240)">
+          <Text textAlign="center" fontSize="xl" fontWeight="400">
+            Credentials
+          </Text>
+          <FormLabel htmlFor="category">New Category</FormLabel>
+          <Input
+            type="text"
+            name="category"
+            id="category"
+            isReadOnly={isEditLocked}
+            onChange={onInputChange}
+          />
+          {categories.map(cat => {
             return <Category key={cat.id} name={cat.name} id={cat.id} />;
           })}
         </Box>
-      </VStack>
+      </Flex>
     </FormControl>
   );
 };
